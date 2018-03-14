c4551e6bf0133f38fcdb2f8e4c36c2160d5f063b
ahmedshoaib <ahmed.shoaib1212@gmail.com>
2018-03-14 14:31:54 +0530

    Creates UI

---
+++ app.js
@@ -0,0 +1,27 @@
+var express = require('express');
+var app = express();
+var path = require('path');
+
+app.listen(8080);
+// viewed at http://localhost:8080
+
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname +'/public'+ '/index.html'));
+});
+
+
+app.get('/signin', function(req, res) {
+    res.sendFile(path.join(__dirname +'/public'+ '/signin.html'));
+});
+
+app.get('/auction', function(req, res) {
+    res.sendFile(path.join(__dirname +'/public'+ '/auction.html'));
+});
+
+app.get('/itemdetails', function(req, res) {
+    res.sendFile(path.join(__dirname +'/public'+ '/itemdetails.html'));
+});
+
+app.get('/additem', function(req, res) {
+    res.sendFile(path.join(__dirname +'/public'+ '/additem.html'));
+});
