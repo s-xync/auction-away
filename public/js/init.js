9e5c2b5371fad182fc82be4429bca8f5e13a16f2
ahmedshoaib <ahmed.shoaib1212@gmail.com>
2018-03-14 14:56:17 +0530

    completes auction page

--- public/js/init.js
+++ public/js/init.js
@@ -1,1 +1,7 @@
-$(".button-collapse").sideNav();
+
+$(document).ready(function(){
+    // the "href" attribute of the modal trigger must specify the modal ID that wants to be triggered
+    $('.modal').modal();
+  });
+
+  $(".button-collapse").sideNav();
