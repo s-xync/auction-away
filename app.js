35663550db9609598dc4c9d06f5b932bc1912eb5
SaiKumar Immadi <immadisaikumar@ymail.com>
2019-01-06 17:57:31 +0530

    mongodb uro

--- app.js
+++ app.js
@@ -1,36 +1,55 @@
-const express = require('express');
-const path = require('path');
-const cookieParser = require('cookie-parser');
-const bodyParser = require('body-parser');
-const exphbs = require('express-handlebars');
-const expressValidator = require('express-validator');
-const flash = require('connect-flash');
-const session = require('express-session');
-const passport = require('passport');
-const LocalStrategy = require('passport-local').Strategy;
-const mongoose = require('mongoose');
-const schedule = require('node-schedule');
+const express = require("express");
+const path = require("path");
+const cookieParser = require("cookie-parser");
+const bodyParser = require("body-parser");
+const exphbs = require("express-handlebars");
+const expressValidator = require("express-validator");
+const flash = require("connect-flash");
+const session = require("express-session");
+const passport = require("passport");
+const LocalStrategy = require("passport-local").Strategy;
+const mongoose = require("mongoose");
+const schedule = require("node-schedule");
 
 // Connect to mongodb using mongoose orm
-if(process.env.MONGODB_PORT_27017_TCP_ADDR && process.env.MONGODB_PORT_27017_TCP_PORT){//container linked with name mongodb
-  mongoose.connect('mongodb://'+process.env.MONGODB_PORT_27017_TCP_ADDR+':'+process.env.MONGODB_PORT_27017_TCP_PORT+'/auctionaway');
-}else{//local system
-  mongoose.connect('mongodb://localhost/auctionaway');
+if (
+  process.env.MONGODB_PORT_27017_TCP_ADDR &&
+  process.env.MONGODB_PORT_27017_TCP_PORT
+) {
+  //container linked with name mongodb
+  mongoose.connect(
+    "mongodb://" +
+      process.env.MONGODB_PORT_27017_TCP_ADDR +
+      ":" +
+      process.env.MONGODB_PORT_27017_TCP_PORT +
+      "/auctionaway"
+  );
+} else if (process.env.MONGODB_URI) {
+  mongoose.connect(process.env.MONGODB_URI);
+} else {
+  //local system
+  mongoose.connect("mongodb://localhost/auctionaway");
 }
-var db=mongoose.connection;
+var db = mongoose.connection;
 
 // Mini Apps
-var routes = require('./routes/index');
-var users = require('./routes/users');
-var auctions=require('./routes/auctions');
+var routes = require("./routes/index");
+var users = require("./routes/users");
+var auctions = require("./routes/auctions");
 
 // Init App
 var app = express();
 
 // View Engine
-app.set('views', path.join(__dirname, 'views'));
-app.engine('handlebars', exphbs({defaultLayout:'layout',helpers: require("./public/js/helpers.js").helpers}));
-app.set('view engine', 'handlebars');
+app.set("views", path.join(__dirname, "views"));
+app.engine(
+  "handlebars",
+  exphbs({
+    defaultLayout: "layout",
+    helpers: require("./public/js/helpers.js").helpers
+  })
+);
+app.set("view engine", "handlebars");
 
 // BodyParser Middleware
 app.use(bodyParser.json());
@@ -38,86 +57,88 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 
 // Set Static Folder
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
 // Express Session
-app.use(session({
-  secret: 'secret',
-  saveUninitialized: true,
-  resave: true
-}));
+app.use(
+  session({
+    secret: "secret",
+    saveUninitialized: true,
+    resave: true
+  })
+);
 
 // Passport init
 app.use(passport.initialize());
 app.use(passport.session());
 
 // Express Validator
-app.use(expressValidator({
-  errorFormatter: function(param, msg, value) {
-    var namespace = param.split('.')
-    , root    = namespace.shift()
-    , formParam = root;
+app.use(
+  expressValidator({
+    errorFormatter: function(param, msg, value) {
+      var namespace = param.split("."),
+        root = namespace.shift(),
+        formParam = root;
 
-    while(namespace.length) {
-      formParam += '[' + namespace.shift() + ']';
+      while (namespace.length) {
+        formParam += "[" + namespace.shift() + "]";
+      }
+      return {
+        param: formParam,
+        msg: msg,
+        value: value
+      };
     }
-    return {
-      param : formParam,
-      msg   : msg,
-      value : value
-    };
-  }
-}));
+  })
+);
 
 // Connect Flash
 app.use(flash());
 
 // Global Vars
-app.use(function (req, res, next) {
-  res.locals.success_msg = req.flash('success_msg');
-  res.locals.error_msg = req.flash('error_msg');
-  res.locals.error = req.flash('error');
+app.use(function(req, res, next) {
+  res.locals.success_msg = req.flash("success_msg");
+  res.locals.error_msg = req.flash("error_msg");
+  res.locals.error = req.flash("error");
   res.locals.user = req.user || null;
   next();
 });
 
 // Logs all requests to the console
-app.use(function (req,res,next){
+app.use(function(req, res, next) {
   console.log(new Date(), req.method, req.url);
   next();
 });
 
 // Routes to Mini Apps
-app.use('/', routes);
-app.use('/users', users);
-app.use('/auctions',auctions);
+app.use("/", routes);
+app.use("/users", users);
+app.use("/auctions", auctions);
 
 // Set Port
-app.set('port', (process.env.PORT || 3000));
-process.title = 'node-app'
-app.listen(app.get('port'), function(){
-  console.log('Server started on port '+app.get('port'));
-  console.log('This process is your pid ' + process.pid);
-  console.log('This process is your process name ' + process.title);
+app.set("port", process.env.PORT || 3000);
+process.title = "node-app";
+app.listen(app.get("port"), function() {
+  console.log("Server started on port " + app.get("port"));
+  console.log("This process is your pid " + process.pid);
+  console.log("This process is your process name " + process.title);
 });
 
-app.get('/error',function(req,res){
+app.get("/error", function(req, res) {
   res.status(404);
-  res.render('error',{title:"404 | Auction Away"});
+  res.render("error", { title: "404 | Auction Away" });
 });
 //this has to be at the end of all other routes
-app.get('*',function (req,res){
-  res.redirect('/error');
+app.get("*", function(req, res) {
+  res.redirect("/error");
 });
 
-const start = schedule.scheduleJob('0 0 0 * * *', function(){
-  console.log('Starting auctions for the day');
-
+const start = schedule.scheduleJob("0 0 0 * * *", function() {
+  console.log("Starting auctions for the day");
 });
 
-const stop = schedule.scheduleJob('59 59 23 * * *', function(){
-  console.log('Stopping auctions for the day');
-
+const stop = schedule.scheduleJob("59 59 23 * * *", function() {
+  console.log("Stopping auctions for the day");
 });
 
 // Auction=require('./models/auction');
